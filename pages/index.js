ee1c3b12f2869f85a37feeca74756897ff20c2f5
opulencedeveloper <victorkudos@gmail.co>
2023-06-14 15:14:31 +0100

    added alt to images

--- pages/index.js
+++ pages/index.js
@@ -13,23 +13,23 @@ import handleWhatsAppClick from "@/helpers/whatsapp";
 import "aos/dist/aos.css";
 
 export default function Home() {
-  useEffect(() => {  
+  useEffect(() => {
     AOS.init();
     AOS.refresh();
   }, []);
   return (
     <>
-    <Image
+      <Image
         src="/images/for-sale.png"
         alt="for sale"
         width={200}
         priority
         loading="eager"
         height={200}
-        onClick={handleWhatsAppClick} 
+        onClick={handleWhatsAppClick}
         className="h-auto w-auto fixed bottom-0 right-10 z-20 cursor-pointer"
-      /> 
-     <SectionOne />
+      />
+      <SectionOne />
       <SectionTwo />
       <SectionThree />
       <SectionFour />
